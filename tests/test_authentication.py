522bc0ad14e5c7df25b2adbba71f17fa83697ce1
Nadyrgulov Timur <nadyrgulov.timur@yandex.ru>
2026-05-07 19:20:43 +0300

    "Написание API автотестов используя Pytest. Практика написания API автотестов"

---
+++ tests/test_authentication.py
@@ -0,0 +1,27 @@
+from http import HTTPStatus
+from clients.authentication.authentication_client import get_authentication_client
+from clients.authentication.authentication_schema import LoginResponseSchema, LoginRequestSchema
+from clients.users.public_users_client import get_public_users_client
+from clients.users.users_schema import CreateUserRequestSchema
+from tools.assertions.authentication import assert_login_response
+from tools.assertions.base import assert_status_code
+from tools.assertions.schema import validate_json_schema
+
+
+def test_login():
+    public_users_client = get_public_users_client()
+    create_request = CreateUserRequestSchema()
+    create_response = public_users_client.create_user(create_request)
+
+    auth_client = get_authentication_client()
+    login_request = LoginRequestSchema(
+        email=create_request.email,
+        password=create_request.password
+    )
+    login_response = auth_client.login_api(login_request)
+    login_data = LoginResponseSchema.model_validate_json(login_response.text)
+
+    # Проверки
+    assert_status_code(login_response.status_code, HTTPStatus.OK)
+    assert_login_response(login_data)
+    validate_json_schema(login_response.json(), LoginResponseSchema.model_json_schema())
