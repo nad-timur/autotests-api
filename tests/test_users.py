d0db7ed82a7379c84ec7e20612dc5c53e22747d0
timur100500 <99761640+timur100500@users.noreply.github.com>
2026-05-08 14:56:23 +0300

    -

--- tests/test_users.py
+++ tests/test_users.py
@@ -3,18 +3,25 @@ import pytest
 
 from clients.users.public_users_client import PublicUsersClient
 from clients.users.private_users_client import PrivateUsersClient
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
-from tools.assertions.users import assert_create_user_response, assert_get_user_response
+from tools.assertions.users import assert_create_user_response
+from tools.fakers import fake
 from tests.conftest import UserFixture
 
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user(public_users_client: PublicUsersClient):  # Используем фикстуру API клиента
-    # Удалили инициализацию API клиента из теста
-    request = CreateUserRequestSchema()
+@pytest.mark.parametrize("email_domain", [
+    "mail.ru",
+    "gmail.com",
+    "example.com"
+])
+def test_create_user(email_domain: str, public_users_client: PublicUsersClient):  # Используем фикстуру API клиента
+
+    email = fake.email(domain=email_domain)
+    request = CreateUserRequestSchema(email=email)
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
