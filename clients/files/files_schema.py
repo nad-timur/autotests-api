f3a04423eb32755eb903604b2326a5570972687e
Nadyrgulov Timur <nadyrgulov.timur@yandex.ru>
2026-05-08 19:02:08 +0300

    "Практикуемся в написании API автотестов. Часть 1. Практика написания негативных API автотестов для работы с файлами"

--- clients/files/files_schema.py
+++ clients/files/files_schema.py
@@ -29,3 +29,11 @@ class CreateFileResponseSchema(BaseModel):
     Описание структуры ответа создания файла.
     """
     file: FileSchema
+
+
+class GetFileResponseSchema(BaseModel):
+    """
+    Описание структуры ответа получения файла.
+    """
+    file: FileSchema
+
--- tools/assertions/base.py
+++ tools/assertions/base.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Sized
 
 
 def assert_status_code(actual: int, expected: int):
@@ -31,7 +31,7 @@ def assert_equal(actual: Any, expected: Any, name: str):
         f'Actual value: {actual}'
     )
 
-    
+
 def assert_is_true(actual: Any, name: str):
     """
     Проверяет, что фактическое значение является истинным.
@@ -43,5 +43,21 @@ def assert_is_true(actual: Any, name: str):
     assert actual, (
         f'Incorrect value: "{name}". '
         f'Expected true value but got: {actual}'
+    )
+
+
+def assert_length(actual: Sized, expected: Sized, name: str):
+    """
+    Проверяет, что длины двух объектов совпадают.
+
+    :param name: Название проверяемого объекта.
+    :param actual: Фактический объект.
+    :param expected: Ожидаемый объект.
+    :raises AssertionError: Если длины не совпадают.
+    """
+    assert len(actual) == len(expected), (
+        f'Incorrect object length: "{name}". '
+        f'Expected length: {len(expected)}. '
+        f'Actual length: {len(actual)}'
     )
 
