211a38991c7cf0d71cd5d163cb091e4164c54f19
Nadyrgulov Timur <nadyrgulov.timur@yandex.ru>
2026-05-10 19:16:03 +0300

    "Настройки автотестов с Pydantic. Практика работы с настройками"

--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -5,6 +5,7 @@ from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema
 from clients.exercises.exercises_client import get_exercises_client, CreateExerciseRequestSchema
+from config import settings
 
 public_users_client = get_public_users_client()
 
@@ -22,7 +23,7 @@ courses_client = get_courses_client(authentication_user)
 exercises_client = get_exercises_client(authentication_user)
 
 # Загружаем файл – указываем только путь к файлу, имя и директория сгенерированы
-create_file_request = CreateFileRequestSchema(upload_file="./testdata/files/image.png")
+create_file_request = CreateFileRequestSchema(upload_file=settings.test_data.image_png_file)
 create_file_response = files_client.create_file(create_file_request)
 print('Create file data:', create_file_response.model_dump())
 
--- tests/files/test_files.py
+++ tests/files/test_files.py
@@ -15,6 +15,7 @@ from tools.assertions.files import assert_create_file_response, assert_get_file_
     assert_create_file_with_empty_filename_response, assert_create_file_with_empty_directory_response, \
     assert_file_not_found_response, assert_get_file_with_incorrect_file_id_response
 from tools.assertions.schema import validate_json_schema
+from config import settings
 
 
 @pytest.mark.files
@@ -31,7 +32,7 @@ class TestFiles:
     @allure.severity(Severity.BLOCKER)
     @allure.sub_suite(AllureStory.CREATE_ENTITY)
     def test_create_file(self, files_client: FilesClient):
-        request = CreateFileRequestSchema(upload_file="./testdata/files/image.png")
+        request = CreateFileRequestSchema(upload_file=settings.test_data.image_png_file)
         response = files_client.create_file_api(request)
         response_data = CreateFileResponseSchema.model_validate_json(response.text)
 
@@ -62,7 +63,7 @@ class TestFiles:
     def test_create_file_with_empty_filename(self, files_client: FilesClient):
         request = CreateFileRequestSchema(
             filename="",
-            upload_file="./testdata/files/image.png"
+            upload_file=settings.test_data.image_png_file
         )
         response = files_client.create_file_api(request)
         response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
@@ -83,7 +84,7 @@ class TestFiles:
     def test_create_file_with_empty_directory(self, files_client: FilesClient):
         request = CreateFileRequestSchema(
             directory="",
-            upload_file="./testdata/files/image.png"
+            upload_file=settings.test_data.image_png_file
         )
         response = files_client.create_file_api(request)
         response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
